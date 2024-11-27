d08c767c3ea106b507a7126e531c8450b4cd78f1
Luis Caldas <augusto@caldas.ie>
2024-11-27 12:55:19 +0000

    Create new spring project

---
+++ Luis_AI-Project/src/test/java/com/sdh4/ai_project/AiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdh4.ai_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
